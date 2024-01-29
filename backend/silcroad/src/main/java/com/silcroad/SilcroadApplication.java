8fc6ce602d106136e02973056af4deb6e3bfc727
gyocheol <kys04171@naver.com>
2024-01-29 12:00:39 +0900

    Backend 설정 및 DB 연동

--- backend/silcroad/src/main/java/com/silcroad/SilcroadApplication.java
+++ backend/silcroad/src/main/java/com/silcroad/SilcroadApplication.java
@@ -1,4 +1,4 @@
-package com.example.silcroad;
+package com.silcroad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SilcroadApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("application","application-db");
 		SpringApplication.run(SilcroadApplication.class, args);
 	}
-
 }
