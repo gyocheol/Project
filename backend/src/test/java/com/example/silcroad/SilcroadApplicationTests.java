7efdc4acd7742445447bf79bb7523003955c5e4f
gyocheol <kys04171@naver.com>
2024-01-29 10:11:32 +0900

    backend

---
+++ backend/src/test/java/com/example/silcroad/SilcroadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.silcroad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SilcroadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
